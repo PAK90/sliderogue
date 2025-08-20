314fb312b4286d12dc18b8079497d67a98ed3e6b
Peter Klavins <paklavins@gmail.com>
2025-08-19 21:31:56 -0600

    Missed extra prop in unused file

--- src/components/GoalChooserDialog.tsx
+++ src/components/GoalChooserDialog.tsx
@@ -54,6 +54,7 @@ const GoalChooserDialog = () => {
                 spell,
                 complete: spell.requiredTiles.map(() => true),
               }}
+              satisfied={true}
             />
           </div>
         ))}
